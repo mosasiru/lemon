2d03fafc6902f088095ac27c0cfc7053e2ea041a
YUITO <dance2982002@hotmail.co.jp>
2012-05-13 00:45:42 +0900

    create model&table option

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :member
   belongs_to :category
   has_many :answers
+  has_many :options
   attr_accessible :category_id, :member_id, :text, :title
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120505025253) do
+ActiveRecord::Schema.define(:version => 20120512153112) do
 
   create_table "answers", :force => true do |t|
     t.integer "question_id"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(:version => 20120505025253) do
     t.integer  "point"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "options", :force => true do |t|
+    t.integer  "question_id"
+    t.string   "string"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "questions", :force => true do |t|
