d92a3cd727adde7e8f5ab980d2b68de05125677e
YUITO <dance2982002@hotmail.co.jp>
2012-06-04 21:30:06 +0900

    質問新規のバリデーション

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -1,4 +1,21 @@
 <!-- 
-var msg = 'テスト！';
-window.alert(msg);
+// var msg = 'テスト！';
+//window.alert(msg);
+
+$(function () {
+	$('form').submit(function (e) {
+		if ( $('#title').val() === '') {
+			window.alert('質問を入力してください。');
+			e.preventDefault();
+		}
+		else if ( $('#option1').val() === '') {
+			window.alert('回答１を入力してください。');
+			e.preventDefault();
+		}
+		else if ( $('#option2').val() === '') {
+			window.alert('回答２を入力してください。');
+			e.preventDefault();
+		}
+	});
+});
 -->
