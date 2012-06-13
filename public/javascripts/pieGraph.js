41574f59bded5506fc60f7d5e9cc13d238f25d1d
YUITO <dance2982002@hotmail.co.jp>
2012-06-14 01:26:56 +0900

    showの、円グラフにstring入れるやつ

--- public/javascripts/pieGraph.js
+++ public/javascripts/pieGraph.js
@@ -1,12 +1,33 @@
+function pieGraphfunction2(opt1,opt2,opt1s,opt2s){
+  var data = [
+      [opt1s,opt1],[opt2s, opt2]]; 
+  plot(data);
+}
 
-function pieGraphfunction(opt1,opt2,opt3,opt4,opt5){
+function pieGraphfunction3(opt1,opt2,opt3,opt1s,opt2s,opt3s){
+  var data = [
+      [opt1s,opt1],[opt2s, opt2], [opt3s, opt3]]; 
+  plot(data);
+}
+
+function pieGraphfunction4(opt1,opt2,opt3,opt4,opt1s,opt2s,opt3s,opt4s){
+  var data = [
+      [opt1s,opt1],[opt2s, opt2], [opt3s, opt3], [opt4s, opt4]] ; 
+  plot(data);
+}
+
+function pieGraphfunction5(opt1,opt2,opt3,opt4,opt5,opt1s,opt2s,opt3s,opt4s,opt5s){
 
   //opt1=<%= @nameOfOption1.string %> dont know why it cant output string??
-
   var data = [
-    ["回答1",opt1],["回答2", opt2], ["回答3", opt3], 
-    ["回答4", opt4],["回答５", opt5]];
-  
+      [opt1s,opt1],[opt2s, opt2], [opt3s, opt3], 
+      [opt4s,opt4],[opt5s, opt5]];
+//    ["回答1",opt1],["回答2", opt2], ["回答3", opt3], 
+//    ["回答4", opt4],["回答５", opt5]];
+  plot(data);  
+}
+
+function plot(data) {
   var plot1 = jQuery.jqplot ('chartDiv', [data], 
     { 
       seriesDefaults: {
@@ -19,7 +40,6 @@ function pieGraphfunction(opt1,opt2,opt3,opt4,opt5){
       legend: { show:true, location: 'e' }
     }
   );
-}
-
-
+	
+};
 
